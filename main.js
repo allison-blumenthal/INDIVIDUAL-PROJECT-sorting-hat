fcf02c5e9628c424e595c05d30d9561f2745e8ba
Allison Blumenthal <blumen.allison@gmail.com>
2022-10-27 07:24:46 -0400

    creating JS code for house assignment

--- main.js
+++ main.js
@@ -42,6 +42,32 @@ const cats = [
   }
 ]
 
+// dynamically rendering to the DOM
+
+const renderToDom = (divId, htmlToRender) => {
+    const selectedDiv = document.querySelector(divId);
+    selectedDiv.innerHTML = htmlToRender;
+};
+
+const catsOnDom = (array) => {
+  let domString = "";
+  for (const cat of array) {
+    domString +=
+      `<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${cat.house}</h5>
+          <p class="card-text">${cat.name}</p>
+          <a href="#" class="btn btn-primary">Expel!</a>
+        </div>
+      </div>`
+    }
+
+    renderToDom("#app", domString);
+}
+
+catsOnDom(cats);
+
+
 // clicking the first button to get to form
 
 const sortMyCatBtn = document.querySelector("#sortMyCatBtn");
@@ -50,8 +76,38 @@ sortMyCatBtn.addEventListender('click', () => {
   // put function to render form here
 })
 
+// form to input name and create new house card 
+
 const form = document.querySelector('form');
 
-const catToSort = {
-  id: cats.length + 1
+const sortCat = (e) => {
+  e.preventDefault();
+
+  const assignHouse = () => {
+    let houseNum = Math.floor(Math.random() * 4) + 1;
+
+    if (houseNum === 1) {
+      return "Gryffindor"
+      } else if (houseNum === 2) {
+      return "Slytherin"
+      } else if (houseNum === 3) {
+      return "Hufflepuff"
+      } else {
+      return "Ravenclaw"
+      }
+  };
+
+  const newCatObj = {
+    id: cats.length + 1,
+    name: document.querySelector("#name").value,
+    house: assignHouse();      
+  }
+
+  cats.push(newCatObj);
+  catsOnDom(cats);
+  form.reset();
 }
+
+const submitButton = document.querySelector("#submit-button");
+
+form.addEventListener('submit', sortCat);
